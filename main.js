9337ba7ebf745774f7079b3397ea6b0d642e5866
AndreasLonn <45557341+AndreasLonn@users.noreply.github.com>
2022-10-02 17:15:08 +0200

    Clean up and add link button

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
-function $(s, e=document) {return e.querySelector(s)}
-function $s(s, e=document) {return e.querySelectorAll(s)}
+function q(query, parent=document) { return parent.querySelector(query); }
+function qs(query, parent=document) { return parent.querySelectorAll(query); }
 
-var ipt = $("input#word-input");
-var word = new URLSearchParams(window.location.search).get("word") || prompt(strings.whatword);
+const ipt = q("input#word-input");
+
+// Base64 encoding to hide the word in the URL (atob: b64 -> text, btoa: text -> b64)
+const word = atob(new URLSearchParams(window.location.search).get("word") || btoa(prompt(strings.whatWord)));
+
 ipt.focus();
 
 var numTries = 0;
 
-$("form#word").addEventListener("submit", e => {
+q("form#word").addEventListener("submit", e => {
     e.preventDefault();
+
     if(ipt.value === word) {
         ipt.value = "";
         numTries = 0;
     }
+
     else {
         ipt.setCustomValidity(strings.incorrect);
         ipt.addEventListener("input", function f() {
@@ -22,8 +27,13 @@ $("form#word").addEventListener("submit", e => {
         numTries++;
     }
 });
+
 ipt.addEventListener("invalid", () => {
-    if(++numTries >= 3 && confirm(strings.moretries))
-        alert(strings.wordwas);
+    if(++numTries >= 3 && confirm(strings.moreTries))
+        alert(strings.wordWas);
 });
-$("form button#forgot").addEventListener("click", () => alert(strings.wordwas));
+
+q("form button#forgot").addEventListener("click", () => alert(strings.wordWas));
+
+q("form button#link").addEventListener("click", () =>
+    prompt(strings.linkToWord, `${window.location.href.split('?')[0]}?word=${btoa(word)}`));
